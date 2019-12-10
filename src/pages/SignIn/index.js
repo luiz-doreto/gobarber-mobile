b9e803b11130b3c2a2bc83a00f354ae8039bda88
Luiz Doreto <luizdoreto@Luizs-MacBook-Air.local>
2019-12-10 20:54:54 -0300

    Estrutura inicial do projeto

---
+++ src/pages/SignIn/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import Background from '~/components/Background';
+import Button from '~/components/Button';
+import Input from '~/components/Input';
+
+export default function SignIn() {
+    return (
+        <Background>
+            <Input
+                style={{ marginTop: 30 }}
+                icon="call"
+                placeholder="testando saporra"
+            />
+            <Button>Entrar</Button>
+        </Background>
+    );
+}
